aa5fc98b15cc48417268b7512455bb6d2c016c98
Rossouw <rossouw.venter.rv@gmail.com>
2021-07-13 09:44:43 +0200

    Fixing Exercise 11

--- Class_Exercise/Class_Exercise/src/main/java/Exercise11/ShoppinCart_3.java
+++ Class_Exercise/Class_Exercise/src/main/java/Exercise11/ShoppinCart_3.java
@@ -15,13 +15,14 @@ public class ShoppinCart_3 {
                     new Item_3("Pants",35.99)};
     
     public static void main(String[] args){
-        ShoppingCart cart = new ShoppingCart();
+        ShoppinCart_3 cart = new ShoppinCart_3();
         cart.displayTotal();
     }
     
     // Use a standard for loop to iterate through the items array, adding up the total price
     //    Skip any items that are back ordered.  Display the Shopping Cart total.
     public void displayTotal(){
+        double total = 0;
         for (int idx = 0; idx < items.length; idx++){
             if (items[idx].isBackOrdered())
                 continue;
